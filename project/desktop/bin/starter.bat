fb48d6d2a8b76d6efdaa8de53de1bd5bbbb169b8
Debabrata Tripathy <dtripathy10@gmail.com>
2015-02-15 19:45:28 +0530

    #version1

--- project/desktop/bin/starter.bat
+++ project/desktop/bin/starter.bat
@@ -5,12 +5,14 @@ PATH=%PATH%;C:\Program Files (x86)\Git\bin
 cd "C:\Users\dtripathy\AppData\Roaming\Sublime Text 3\Packages"
 
 :: Install soda-theme
-git clone https://github.com/buymeasoda/soda-theme/ "Theme - Soda"
+:: git clone https://github.com/buymeasoda/soda-theme/ "Theme - Soda"
 
 :: Install GAMS
-git clone https://github.com/lolow/sublime-gams.git GAMS
+:: git clone https://github.com/lolow/sublime-gams.git GAMS
 
 :: Install Git
-git clone https://github.com/jisaacks/GitGutter.git
-git clone https://github.com/kemayo/sublime-text-git.git
+:: git clone https://github.com/jisaacks/GitGutter.git
+:: git clone https://github.com/kemayo/sublime-text-git.git
+
+git clone https://github.com/titoBouzout/SideBarEnhancements.git
 
