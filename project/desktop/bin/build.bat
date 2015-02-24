e35e1139c94beb5142239c8f036533d640a796ab
Debabrata Tripathy <dtripathy10@gmail.com>
2015-02-24 20:25:49 +0530

    Update build.bat

--- project/desktop/bin/build.bat
+++ project/desktop/bin/build.bat
@@ -15,6 +15,7 @@ PATH=%PATH%;C:\Program Files (x86)\Git\bin;C:\Python27
 :: git config --global user.email "dtripathy10@gmail.com"
 :: git config --global user.name "Debabrata Tripathy"
 
+:: setx -m JAVA_HOME "C:\Program Files (x86)\Java\jdk1.7.0_04"
 
 :: git init && git add . && git commit -m "testint"
 ECHO "%CD%"
